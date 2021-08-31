40f45bf8f41c7b984e3eaae50544caddaf9dbd9f
unknown <hsc.jr94@gmail.com>
2021-08-31 15:32:04 -0300

    Validação e campo de registros feitos

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +8,49 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPage implements OnInit {
   authForm: FormGroup;
+  configs = {
+    isSignIn: true,
+    action: 'Login',
+    actionChange: 'Criar conta'
+  };
+
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.createForm();
   }
-  private createForm(): void{
+  private createForm(): void {
     this.authForm = this.fb.group({
-      email: ['',[Validators.required , Validators.email]],
-
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     })
+  }
+
+
+  get email(): FormControl {
+    return <FormControl>this.authForm.get('email');
+  }
+  get password(): FormControl {
+    return <FormControl>this.authForm.get('password');
+  }
+  get name(): FormControl {
+    return <FormControl>this.authForm.get('name');
+  }
+
+
+  changeAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Login' : 'Criar conta';
+    this.configs.actionChange = isSignIn ? 'Criar conta' : 'Já tenho uma conta';
+    !isSignIn
+      ? this.authForm.addControl('name', this.nameControl)
+      : this.authForm.removeControl('name');
+  }
+
+  onSubmit(): void {
+    console.log("AuthForm : ", this.authForm.value);
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  exports: [CommonModule, FormsModule, IonicModule]
+  exports: [CommonModule, ReactiveFormsModule, IonicModule]
 })
 export class SharedModule { }
